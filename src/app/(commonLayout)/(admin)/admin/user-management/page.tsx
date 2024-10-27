5caeb03a022f408f8becbb285960529345b4fa53
Zillur2023 <zillurrahmanbd12@gmail.com>
2024-10-27 20:34:24 +0600

    user-management antd table replace by nextUi table

--- src/app/(commonLayout)/(admin)/admin/user-management/page.tsx
+++ src/app/(commonLayout)/(admin)/admin/user-management/page.tsx
@@ -1,21 +1,84 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable react-hooks/exhaustive-deps */
 'use client'
-import React, { useEffect, useRef, useState } from 'react';
-import { Table, Tag, Input, Button, Space, InputRef } from 'antd';
-import { SearchOutlined } from '@ant-design/icons';
-import type { ColumnsType } from 'antd/es/table';
-import type { FilterDropdownProps } from 'antd/es/table/interface';
-import Highlighter from 'react-highlight-words';
-import { IUser } from '@/components/type';
+import React, { useEffect, useState } from 'react';
+import {
+  Table,
+  TableHeader,
+  TableColumn,
+  TableBody,
+  TableRow,
+  TableCell,
+  Input,
+  Button,
+  DropdownTrigger,
+  Dropdown,
+  DropdownMenu,
+  DropdownItem,
+  Chip,
+  Pagination,
+  Selection,
+  ChipProps,
+  SortDescriptor
+} from "@nextui-org/react";
+
 import { useGetAllUserQuery } from '@/redux/features/user/userApi';
+import { VerticalDotsIcon } from '@/components/table/VerticalDotsIcon';
+import { ChevronDownIcon } from '@/components/table/ChevronDownIcon';
+import { PlusIcon } from '@/components/table/PlusIcon';
+import { capitalize } from '@/components/table/utils';
+import {SearchIcon} from '@/components/table/SearchIcon'
 import Author from '@/components/shared/Author';
 
-const UserManagement: React.FC = () => {
+export interface IUser {
+  _id?: string;
+  name: string;
+  email: string;
+  image: string;
+  followers: number; 
+  following: number; 
+  isVerified: boolean;
+  role: 'admin' | 'user'; // Role can be either 'admin' or 'user'
+  paymentStatus?: 'Pending' | 'Paid' | 'Failed';
+  transactionId?: string;
+
+}
+
+const columns = [
+  {name: "AUTHOR", uid: "name", sortable: true},
+  {name: "EMAIL", uid: "email"},
+  {name: "FOLLOWERS", uid: "followers", sortable: true},
+  {name: "FOLLOWING", uid: "following", sortable: true},
+  {name: "VERIFIED", uid: "isVerified"},
+  {name: "ROLE", uid: "role", sortable: true},
+  {name: "PAYMENT STATUS", uid: "paymentStatus"},
+  {name: "TRANSACTION ID", uid: "transactionId"},
+  {name: "ACTIONS", uid: "actions"},
+];
+
+  const roleOptions = [
+    {name: "Admin", uid: "admin"},
+    {name: "User", uid: "user"},
+    
+  ]
+
+  const roleColorMap: Record<string, ChipProps["color"]> = {
+    admin: "success",
+    // paused: "danger",
+    user: "warning",
+  };
+  // const verifiedColorMap: Record<string, ChipProps["color"]> = {
+  //   isVerifiled: "success",
+  //   // paused: "danger",
+  //   // user: "warning",
+  // };
+  
+  const INITIAL_VISIBLE_COLUMNS = ["name", "role", "followers", "following", "isVerified", "paymentStatus", "actions"];
+
+const UserManagementPage = () => {
   const { data: allUser, refetch } = useGetAllUserQuery('');
   const [users, setUsers] = useState<IUser[]>(allUser?.data || []);
-  const [searchText, setSearchText] = useState('');
-  const [searchedColumn, setSearchedColumn] = useState('');
-  const searchInput = useRef<InputRef>(null);
+  console.log({users})
+
 
   // Update users state when allUser data changes
   useEffect(() => {
@@ -24,184 +87,321 @@ const UserManagement: React.FC = () => {
     }
   }, [allUser?.data, refetch]);
 
-  // Handle the search functionality
-  const handleSearch = (
-    selectedKeys: string[],
-    confirm: FilterDropdownProps['confirm'],
-    dataIndex: 'name' | 'email',
-  ) => {
-    confirm();
-    setSearchText(selectedKeys[0]);
-    setSearchedColumn(dataIndex);
-  };
-
-  const handleReset = (clearFilters: () => void) => {
-    clearFilters();
-    setSearchText('');
-  };
-
-  // Search properties for "User" column (both name and email)
-  const getColumnSearchProps = (): any => ({
-    filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters }: FilterDropdownProps) => (
-      <div style={{ padding: 8 }} onKeyDown={(e) => e.stopPropagation()}>
-        <Input
-          ref={searchInput}
-          placeholder="Search user (name or email)"
-          value={selectedKeys[0]}
-          onChange={(e) => setSelectedKeys(e.target.value ? [e.target.value] : [])}
-          onPressEnter={() => handleSearch(selectedKeys as string[], confirm, 'name')}
-          style={{ marginBottom: 8, display: 'block' }}
-        />
-        <Space>
-          <Button
-            type="primary"
-            onClick={() => handleSearch(selectedKeys as string[], confirm, 'name')}
-            icon={<SearchOutlined />}
-            size="small"
-            style={{ width: 90 }}
-          >
-            Search
-          </Button>
-          <Button
-            onClick={() => clearFilters && handleReset(clearFilters)}
-            size="small"
-            style={{ width: 90 }}
-          >
-            Reset
-          </Button>
-        </Space>
-      </div>
-    ),
-    filterIcon: (filtered: boolean) => (
-      <SearchOutlined style={{ color: filtered ? '#1677ff' : undefined }} />
-    ),
-    onFilter: (value: string, record: IUser) => {
-      const fullNameEmail = `${record.name} ${record.email}`.toLowerCase();
-      return fullNameEmail.includes(value.toLowerCase());
-    },
-    onFilterDropdownOpenChange: (visible: boolean) => {
-      if (visible) {
-        setTimeout(() => searchInput.current?.select(), 100);
-      }
-    },
-    render: (text: string | undefined, record: IUser) => {
-      const isSearching = searchedColumn === 'name' || searchedColumn === 'email';
-
-      return (
-        <Author
-          author={{
-            ...record,
-            name: isSearching ? (
-              <Highlighter
-                highlightStyle={{ backgroundColor: '#ffc069', padding: 0 }}
-                searchWords={[searchText]}
-                autoEscape
-                textToHighlight={record.name || ''}
-              />
-            ) : (
-              record.name
-            ),
-            email: isSearching ? (
-              <Highlighter
-                highlightStyle={{ backgroundColor: '#ffc069', padding: 0 }}
-                searchWords={[searchText]}
-                autoEscape
-                textToHighlight={record.email || ''}
-              />
-            ) : (
-              record.email
-            ),
-          }}
-          nameClass={'font-medium'} // Pass the highlight class if necessary
-        />
-      );
-    },
+  const [filterValue, setFilterValue] = React.useState("");
+  const [selectedKeys, setSelectedKeys] = React.useState<Selection>(new Set([]));
+  const [visibleColumns, setVisibleColumns] = React.useState<Selection>(new Set(INITIAL_VISIBLE_COLUMNS));
+  const [roleFilter, setRoleFilter] = React.useState<Selection>("all");
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+  const [sortDescriptor, setSortDescriptor] = React.useState<SortDescriptor>({
+    column: "followers",
+    direction: "ascending",
   });
+  const [page, setPage] = React.useState(1);
 
-  // Define columns for the table
-  const columns: ColumnsType<IUser> = [
-    {
-      title: 'User',
-      key: 'user',
-      ...getColumnSearchProps(), // Apply search for both name and email
-    },
-    {
-      title: 'Followers',
-      dataIndex: 'followers',
-      key: 'followers',
-      sorter: (a, b) => a.followers - b.followers,
-    },
-    {
-      title: 'Following',
-      dataIndex: 'following',
-      key: 'following',
-      sorter: (a, b) => a.following - b.following,
-    },
-    {
-      title: 'Verified',
-      dataIndex: 'isVerified',
-      key: 'isVerified',
-      render: (isVerified: boolean) => (
-        isVerified ? <Tag color="green">Verified</Tag> : <Tag color="red">Not Verified</Tag>
-      ),
-      filters: [
-        { text: 'Verified', value: true },
-        { text: 'Not Verified', value: false },
-      ],
-      onFilter: (value, record) => record.isVerified === value,
-    },
-    {
-      title: 'Role',
-      dataIndex: 'role',
-      key: 'role',
-      render: (role: 'admin' | 'user') => (
-        role === 'admin' ? <Tag color="blue">Admin</Tag> : <Tag color="gold">User</Tag>
-      ),
-      filters: [
-        { text: 'Admin', value: 'admin' },
-        { text: 'User', value: 'user' },
-      ],
-      onFilter: (value, record) => record.role === value,
-    },
-    {
-      title: 'Payment Status',
-      dataIndex: 'paymentStatus',
-      key: 'paymentStatus',
-      render: (paymentStatus: 'Pending' | 'Paid' | 'Failed') => (
-        <Tag color={paymentStatus === 'Paid' ? 'green' : paymentStatus === 'Pending' ? 'orange' : 'red'}>
-          {paymentStatus}
-        </Tag>
-      ),
-      filters: [
-        { text: 'Pending', value: 'Pending' },
-        { text: 'Paid', value: 'Paid' },
-        { text: 'Failed', value: 'Failed' },
-      ],
-      onFilter: (value, record) => record.paymentStatus === value,
-    },
-    {
-      title: 'Transaction ID',
-      dataIndex: 'transactionId',
-      key: 'transactionId',
-      render: (transactionId: string) => (
-        <span style={{ color: transactionId ? 'blue' : 'grey' }}>
-          {transactionId || 'N/A'}
+  const pages = Math.ceil(users.length / rowsPerPage);
+
+  const hasSearchFilter = Boolean(filterValue);
+
+  const headerColumns = React.useMemo(() => {
+    if (visibleColumns === "all") return columns;
+
+    return columns.filter((column) => Array.from(visibleColumns).includes(column.uid));
+  }, [visibleColumns]);
+
+  const filteredItems = React.useMemo(() => {
+    let filteredUsers = [...users];
+
+    if (hasSearchFilter) {
+      filteredUsers = filteredUsers.filter((user) =>
+        user.name.toLowerCase().includes(filterValue.toLowerCase()),
+      );
+    }
+    if (roleFilter !== "all" && Array.from(roleFilter).length !== roleOptions.length) {
+      filteredUsers = filteredUsers.filter((user) =>
+        Array.from(roleFilter).includes(user?.role),
+      );
+    }
+
+    return filteredUsers;
+  }, [users, filterValue, roleFilter]);
+
+  const items = React.useMemo(() => {
+    const start = (page - 1) * rowsPerPage;
+    const end = start + rowsPerPage;
+
+    return filteredItems.slice(start, end);
+  }, [page, filteredItems, rowsPerPage]);
+
+  const sortedItems = React.useMemo(() => {
+    return [...items].sort((a: IUser, b: IUser) => {
+      const first = a[sortDescriptor.column as keyof IUser] as number;
+      const second = b[sortDescriptor.column as keyof IUser] as number;
+      const cmp = first < second ? -1 : first > second ? 1 : 0;
+
+      return sortDescriptor.direction === "descending" ? -cmp : cmp;
+    });
+  }, [sortDescriptor, items]);
+
+  const renderCell = React.useCallback((user: IUser, columnKey: React.Key) => {
+    const cellValue = user[columnKey as keyof IUser];
+
+    switch (columnKey) {
+      case "name":
+        return (
+          // <User
+          //   avatarProps={{radius: "full", size: "sm", src: user?.image}}
+          //   classNames={{
+          //     description: "text-default-500",
+          //   }}
+          //   description={user.email}
+          //   name={cellValue}
+          // >
+          //   {user.email}
+          // </User>
+          <Author author={user}/>
+        );
+      case "role":
+        return (
+          <Chip
+            className="capitalize border-none gap-1 text-default-600"
+            color={roleColorMap[user?.role]}
+            size="sm"
+            variant="dot"
+          >
+            {cellValue}
+          </Chip>
+        );
+      case "isVerified":
+        return (
+          <p> {(cellValue as boolean) && <span className=' border rounded-md px-2 py-1 text-blue-400'>{'Verified'}</span>} </p>
+        );
+      case "actions":
+        return (
+          <div className="relative flex justify-end items-center gap-2">
+            <Dropdown className="bg-background border-1 border-default-200">
+              <DropdownTrigger>
+                <Button isIconOnly radius="full" size="sm" variant="light">
+                  <VerticalDotsIcon className="text-default-400" width={24} height={24} />
+                </Button>
+              </DropdownTrigger>
+              <DropdownMenu>
+                <DropdownItem>View</DropdownItem>
+                <DropdownItem>Edit</DropdownItem>
+                <DropdownItem>Delete</DropdownItem>
+              </DropdownMenu>
+            </Dropdown>
+          </div>
+        );
+      default:
+        return cellValue;
+    }
+  }, []);
+  
+
+  const onRowsPerPageChange = React.useCallback((e: React.ChangeEvent<HTMLSelectElement>) => {
+    setRowsPerPage(Number(e.target.value));
+    setPage(1);
+  }, []);
+
+  const onSearchChange = React.useCallback((value?: string) => {
+    if (value) {
+      setFilterValue(value);
+      setPage(1);
+    } else {
+      setFilterValue("");
+    }
+  }, []);
+
+  const topContent = React.useMemo(() => {
+    return (
+      <div className="flex flex-col gap-4">
+        <div className="flex justify-between gap-3 items-end">
+          <Input
+            isClearable
+            classNames={{
+              base: "w-full sm:max-w-[44%]",
+              inputWrapper: "border-1",
+            }}
+            placeholder="Search by name..."
+            size="sm"
+            startContent={<SearchIcon className="text-default-300" />}
+            value={filterValue}
+            variant="bordered"
+            onClear={() => setFilterValue("")}
+            onValueChange={onSearchChange}
+          />
+          <div className="flex gap-3">
+            <Dropdown>
+              <DropdownTrigger className="hidden sm:flex">
+                <Button
+                  endContent={<ChevronDownIcon className="text-small" />}
+                  size="sm"
+                  variant="flat"
+                >
+                  Role
+                </Button>
+              </DropdownTrigger>
+              <DropdownMenu
+                disallowEmptySelection
+                aria-label="Table Columns"
+                closeOnSelect={false}
+                selectedKeys={roleFilter}
+                selectionMode="multiple"
+                onSelectionChange={setRoleFilter}
+              >
+                {roleOptions.map((role) => (
+                  <DropdownItem key={role.uid} className="capitalize">
+                    {capitalize(role.name)}
+                  </DropdownItem>
+                ))}
+              </DropdownMenu>
+            </Dropdown>
+            <Dropdown>
+              <DropdownTrigger className="hidden sm:flex">
+                <Button
+                  endContent={<ChevronDownIcon className="text-small" />}
+                  size="sm"
+                  variant="flat"
+                >
+                  Columns
+                </Button>
+              </DropdownTrigger>
+              <DropdownMenu
+                disallowEmptySelection
+                aria-label="Table Columns"
+                closeOnSelect={false}
+                selectedKeys={visibleColumns}
+                selectionMode="multiple"
+                onSelectionChange={setVisibleColumns}
+              >
+                {columns.map((column) => (
+                  <DropdownItem key={column.uid} className="capitalize">
+                    {capitalize(column.name)}
+                  </DropdownItem>
+                ))}
+              </DropdownMenu>
+            </Dropdown>
+            <Button
+              className="bg-foreground text-background"
+              endContent={<PlusIcon width={24} height={24} />}
+              size="sm"
+            >
+              Add New
+            </Button>
+          </div>
+        </div>
+        <div className="flex justify-between items-center">
+          <span className="text-default-400 text-small">Total {users.length} users</span>
+          <label className="flex items-center text-default-400 text-small">
+            Rows per page:
+            <select
+              className="bg-transparent outline-none text-default-400 text-small"
+              onChange={onRowsPerPageChange}
+            >
+              <option value="5">5</option>
+              <option value="10">10</option>
+              <option value="15">15</option>
+            </select>
+          </label>
+        </div>
+      </div>
+    );
+  }, [
+    filterValue,
+    roleFilter,
+    visibleColumns,
+    onSearchChange,
+    onRowsPerPageChange,
+    users.length,
+    hasSearchFilter,
+  ]);
+
+  const bottomContent = React.useMemo(() => {
+    return (
+      <div className="py-2 px-2 flex justify-between items-center">
+        <Pagination
+          showControls
+          classNames={{
+            cursor: "bg-foreground text-background",
+          }}
+          color="default"
+          isDisabled={hasSearchFilter}
+          page={page}
+          total={pages}
+          variant="light"
+          onChange={setPage}
+        />
+        <span className="text-small text-default-400">
+          {selectedKeys === "all"
+            ? "All items selected"
+            : `${selectedKeys.size} of ${items.length} selected`}
         </span>
-      ),
-    },
-  ];
+      </div>
+    );
+  }, [selectedKeys, items.length, page, pages, hasSearchFilter]);
+
+  const classNames = React.useMemo(
+    () => ({
+      wrapper: ["max-h-[382px]", "max-w-3xl"],
+      th: ["bg-transparent", "text-default-500", "border-b", "border-divider"],
+      td: [
+        // changing the rows border radius
+        // first
+        "group-data-[first=true]:first:before:rounded-none",
+        "group-data-[first=true]:last:before:rounded-none",
+        // middle
+        "group-data-[middle=true]:before:rounded-none",
+        // last
+        "group-data-[last=true]:first:before:rounded-none",
+        "group-data-[last=true]:last:before:rounded-none",
+      ],
+    }),
+    [],
+  );
 
   return (
-    <div>
-      <h1>User List</h1>
-      <Table<IUser>
-        columns={columns}
-        dataSource={users}
-        rowKey={(record) => record._id!}
-        loading={!allUser} // Show loading if data is not yet loaded
-      />
-    </div>
+    <Table
+      isCompact
+      removeWrapper
+      aria-label="Example table with custom cells, pagination and sorting"
+      bottomContent={bottomContent}
+      bottomContentPlacement="outside"
+      checkboxesProps={{
+        classNames: {
+          wrapper: "after:bg-foreground after:text-background text-background",
+        },
+      }}
+      classNames={classNames}
+      selectedKeys={selectedKeys}
+      selectionMode="multiple"
+      sortDescriptor={sortDescriptor}
+      topContent={topContent}
+      topContentPlacement="outside"
+      onSelectionChange={setSelectedKeys}
+      onSortChange={setSortDescriptor}
+    >
+      <TableHeader columns={headerColumns}>
+        {(column) => (
+          <TableColumn
+            key={column.uid}
+            align={column.uid === "actions" ? "center" : "start"}
+            allowsSorting={column.sortable}
+          >
+            {column.name}
+          </TableColumn>
+        )}
+      </TableHeader>
+      <TableBody emptyContent={"No data found"} items={sortedItems}>
+        {(item) => (
+          <TableRow key={item?._id}>
+            {(columnKey) => <TableCell>{renderCell(item, columnKey)}</TableCell>}
+          </TableRow>
+        )}
+      </TableBody>
+    </Table>
   );
-};
+}
 
-export default UserManagement;
+export default UserManagementPage;
+
+
