18fbab64ae47aff7172566be9ee68f61eceb8e3c
Zillur2023 <zillurrahmanbd12@gmail.com>
2024-10-27 19:18:19 +0600

    schemas add for login and register page

---
+++ src/components/schemas/login.schema.ts
@@ -0,0 +1,11 @@
+import { z } from "zod";
+
+const loginValidationSchema = z.object({
+  email: z.string().trim().email("Please enter a valid email"),
+  password: z
+    .string()
+    .trim()
+    .min(4, "Password needs to be at lest 4 character"),
+});
+
+export default loginValidationSchema;
